bb592c9472d94489cd1282b2ef5c8dea53415945
tomomooza <tomontomonlearner@gmail.com>
2021-05-12 13:05:40 +0900

    お料理登録画面のお料理をDBに登録する処理を作成しました

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use Auth;
 use App\Ingredient;
 use App\Season;
+use App\Dish;
+use App\Manage_ingredient;
+use Illuminate\Support\Facades\DB;
 
 class DishController extends Controller
 {
@@ -93,6 +96,116 @@ class DishController extends Controller
             return redirect()->action('DishController@index');
         }
 
+        //他人のお料理を検索した場合ー＞新規登録になる。
+        //自分のお料理を検索した場合ー＞変更になる。
+        //検索せずに、新規入力した場合ー＞新規登録になる。
+        //自分のお料理に関しては、料理名の重複は不可とする。
+        if ($request->filled('dishes_id')) {
+            //更新
+            $dishes_db = Dish::where('user_id', Auth::user()->id)->where('dish_name', $request->dish_name)->first();
+            if ($dishes_db->id !=$request->dishes_id) {
+                $errors[] = 'お料理名が重複しています。別の名前にしてください';
+                session(['errors' =>$errors]);
+                return redirect()->action('DishController@index');
+            }
+            $dishes_db = Dish::find($request->dishes_id);
+            Manage_ingredient::where('dishes_id', $request->dish_id)->delete();
+        } else {
+            //新規登録
+            $dishes_db = Dish::where('user_id', Auth::user()->id)->where('dish_name', $request->dish_name)->first();
+            if (!($dishes_db == NULL)) {
+                $errors[] = 'お料理名が重複しています。別の名前にしてください。';
+                session(['errors' => $errors]);
+                return redirect()->action('DishContoller@index');
+            }
+            $dishes_db = new Dish;
+            $dishes_db['user_id'] = Auth::user()->id;
+        }
+        $dishes_db ['dish_name'] = $request->dish_name;
+        if ($request->filled('white') && $request->white == '1') {
+            $dishes_db['white'] = 1;
+        } else {
+            $dishes_db['white'] = 0;
+        }
+        if ($request->filled('pink') && $request->pink == '1') {
+            $dishes_db['pink'] = 1;
+        } else {
+            $dishes_db['pink'] = 0;
+        }
+        if ($request->filled('red') && $request->red == '1') {
+            $dishes_db['red'] = 1;
+        } else {
+            $dishes_db['red'] = 0;
+        }
+        if ($request->filled('green') && $request->green == '1') {
+            $dishes_db['green'] = 1;
+        } else {
+            $dishes_db['green'] = 0;
+        }
+        if ($request->filled('yellowish_green') && $request->yellowish_green == '1') {
+            $dishes_db['yellowish_green'] = 1;
+        } else {
+            $dishes_db['yellowish_green'] = 0;
+        }
+        if ($request->filled('yellow') && $request->yellow == '1') {
+            $dishes_db['yellow'] = 1;
+        } else {
+            $dishes_db['yellow'] = 0;
+        }
+        if ($request->filled('beige') && $request->beige == '1') {
+            $dishes_db['beige'] = 1;
+        } else {
+            $dishes_db['beige'] = 0;
+        }
+        if ($request->filled('orange') && $request->orange == '1') {
+            $dishes_db['orange'] = 1;
+        } else {
+            $dishes_db['orange'] = 0;
+        }
+        if ($request->filled('brown') && $request->brown == '1') {
+            $dishes_db['brown'] = 1;
+        } else {
+            $dishes_db['brown'] = 0;
+        }
+        if ($request->filled('purple') && $request->purple == '1') {
+            $dishes_db['purple'] = 1;
+        } else {
+            $dishes_db['purple'] = 0;
+        }
+        if ($request->filled('black') && $request->black == '1') {
+            $dishes_db['black'] = 1;
+        } else {
+            $dishes_db['black'] = 0;
+        }
+        $dishes_db['seasoning'] = $request->seasoning;
+        if ($request->filled('memo')) {
+            $dishes_db['memo'] = $request->memo;
+        } else {
+            $dishes_db['memo'] = '';
+        }
+        $dishes_db['public_private'] = $request->public_private;
+
+        DB::beginTransaction();
+        try {
+            $dishes_db-> save();
+            if ($request->filled('dishes_id')) {
+                //更新
+                Manage_ingredient::where('dishes_id', $request->dishes_id)->delete();
+            }  
+            foreach ($request->ingredients as $v) {
+                $manage_ingredients_db = new Manage_ingredient;
+                $manage_ingredients_db['dishes_id'] = $dishes_db->id;
+                $manage_ingredients_db['ingredients_id'] = $v;
+                $manage_ingredients_db->save();
+            }
+            DB::commit();
+            $errors[] = 'お料理の登録をしました';
+        } catch (\Exception $e) {
+            DB::rollback();
+            $errors[] = 'データ更新でエラーが発生しました。管理者にお問い合わせ下さい';
+        }
+
+        session(['errors' => $errors]);
         return redirect()->action('DishController@index');
     }
 
